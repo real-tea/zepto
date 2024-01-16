4278a381b9db034312879eee201243997d8a46aa
BSF <bsf@Akashs-Macbook-Air.local>
2024-01-16 11:51:09 +0530

    files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
+/* eslint-disable no-use-before-define */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import top100Films from './data';  //hard coded data for testing
+import './styles/styles.css'
 
-function App() {
+// Styles for the Autocomplete component
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: 500,
+      '& > * + *': {
+        marginTop: theme.spacing(3),
+      },
+    },
+  }),
+);
+
+// Main component
+export default function LimitTags() {
+  const classes = useStyles();
+
+  // State to track selected options
+  const [selectedOptions, setSelectedOptions] = React.useState([
+    top100Films[13],
+    top100Films[12],
+    top100Films[11],
+  ]);
+
+  // Filter options to exclude those that are already selected
+  const filteredOptions = top100Films.filter(
+    (option) => !selectedOptions.find((selected) => selected.title === option.title)
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${classes.root} center-container`}>
+      {/* Autocomplete component */}
+      <Autocomplete
+        multiple
+        limitTags={2}
+        id="multiple-limit-tags"
+        options={filteredOptions}
+        getOptionLabel={(option) => option.title}
+        value={selectedOptions}
+        onChange={(event, newValue) => setSelectedOptions(newValue)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            label="Best Movies"
+            placeholder="Favorites"
+            className="autocomplete-input"
+          />
+        )}
+        className="autocomplete-container"
+        PaperComponent={({ children }) => (
+          <Paper className="autocomplete-dropdown">{children}</Paper>
+        )}
+        classes={{
+          option: 'autocomplete-option',
+        }}
+      />
     </div>
   );
 }
 
-export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+  // ... (existing config)
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+  ],
+}
